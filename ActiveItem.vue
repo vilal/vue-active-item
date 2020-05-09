2d1c4446bba475dff3c8b8748001f92af8609232
vilal <lalandev@free.fr>
2020-05-09 15:55:27 +0200

    init

---
+++ ActiveItem.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    originalActive: {
+      type: [Boolean, Object, Number],
+      default: false
+    }
+  },
+  data() {
+    return {
+      active: this.originalActive
+    };
+  },
+  methods: {
+    setActive(active) {
+      this.active = active;
+    }
+  },
+  render() {
+    return this.$scopedSlots.default({
+      setActive: this.setActive,
+      active: this.active
+    });
+  }
+};
+</script>
